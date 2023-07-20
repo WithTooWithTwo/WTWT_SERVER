d737169cad491b9d8087e2dbc6cabd3643d98fd0
uijin-j <uijin0531@gmail.com>
2023-07-21 03:22:20 +0900

    chore: Spring Boot 프로젝트 생성

---
+++ wtwt/src/main/java/cupitoo/wtwt/WtwtApplication.java
@@ -0,0 +1,13 @@
+package cupitoo.wtwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtwtApplication.class, args);
+	}
+
+}
---
+++ wtwt/src/test/java/cupitoo/wtwt/WtwtApplicationTests.java
@@ -0,0 +1,13 @@
+package cupitoo.wtwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
