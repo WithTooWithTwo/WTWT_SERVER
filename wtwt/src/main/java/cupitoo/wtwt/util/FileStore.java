12e61d653d27f4b44d9febcb1a8b0d3cbd971dad
uijin-j <uijin0531@gmail.com>
2023-07-21 06:42:13 +0900

    feat: 파일스토어 생성

---
+++ wtwt/src/main/java/cupitoo/wtwt/util/FileStore.java
@@ -0,0 +1,58 @@
+package cupitoo.wtwt.util;
+
+import cupitoo.wtwt.model.Image;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+    @Value("${spring.file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public List<Image> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<Image> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public Image storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile == null) {
+            return null;
+        }
+
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new Image(storeFileName);
+    }
+
+    private String createStoreFileName(String originalFilename) { //uuid로 서버에 저장될 파일명 생성
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) { //png와 같은 확장자 추출!
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+}
